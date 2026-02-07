ddf965a645dfa76bdb64ed6a25596899bbeabc05
Oleksandr Tunik <70912192+Smegalex@users.noreply.github.com>
2026-02-07 21:36:09 +0000

    Computer and FileSystemnode WIP

---
+++ Assets/Computer/FileSystemNode.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Computer
+{
+    class FileSystemNode
+    {
+        public string Name;
+        public string Path = "/"; // Path always ends in "/"
+        public ArrayList<FileSystemNode> Children;
+        public File Contents;
+        public FileSystemNode(string path, string name) { 
+            this.Path = path;
+            this.Name = name;
+        }
+
+        public FileSystemNode(string path, string name, File file)
+        {
+            this.Path = path;
+            this.Name = name;
+            this.Contents = file;
+        }
+
+        public FileSystemNode(string path, string name, ArrayList<FileSystemNode> children)
+        {
+            this.Path = path;
+            this.Name = name;
+            this.Children = children;
+        }
+
+        public GetPath()
+        {
+            return Path + Name + "/"; // Path always ends in "/"
+        }
+    }
+}
