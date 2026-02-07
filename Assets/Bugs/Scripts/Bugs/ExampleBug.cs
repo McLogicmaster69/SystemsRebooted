e5b50d658a7ddab50cfa666c0563ac0cc4d5ee9b
Jacob Warren <logicmaster5823@gmail.com>
2026-02-07 21:25:50 +0000

    Basic Bugs

---
+++ Assets/Bugs/Scripts/Bugs/ExampleBug.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace SystemReboot.Bugs
+{
+    public class ExampleBug : Bug
+    {
+        public override void InitBug()
+        {
+            
+        }
+
+        public override bool CheckComplete()
+        {
+            return true;
+        }
+    }
+}
