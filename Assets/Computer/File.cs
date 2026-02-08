2125aceedb7fec69fbe5e76b8ace40ef8198f211
Jacob Warren <45540656+McLogicmaster69@users.noreply.github.com>
2026-02-08 00:13:20 +0000

    Computer (#5)
    * Computer and FileSystemnode WIP
    
    * Kinda finished folder work stumps
    
    ---------
    
    Co-authored-by: Oleksandr Tunik <70912192+Smegalex@users.noreply.github.com>

---
+++ Assets/Computer/File.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Computer
+{
+    public class File
+    {
+        public string Name;
+        public string Path;
+        public File(string path, string name)
+        {
+            this.Name = name;
+            this.Path = path;
+        }
+
+
+    }
+}
