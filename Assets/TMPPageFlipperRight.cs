c3e079b29ee3db879202370cdfbb614ab6037677
AlMaraghi777 <aliwaelmaraghi@gmail.com>
2026-02-08 00:17:25 +0000

    fixed first page of manual

--- Assets/TMPPageFlipperRight.cs
+++ Assets/TMPPageFlipperRight.cs
@@ -1,7 +1,7 @@
 using TMPro;
 using UnityEngine;
 
-public class TMPPageFlipper : MonoBehaviour
+public class TMPPageFlipperRight : MonoBehaviour
 {
     public TextMeshProUGUI text;
     private int currentPage = 0;
